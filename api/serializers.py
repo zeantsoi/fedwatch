a85df0aa8a705cf09546d233d2e94b835e447772
zeantsoi <zeantsoi@gmail.com>
2015-01-24 14:49:41 -0700

    Adding hyperlink

--- api/serializers.py
+++ api/serializers.py
@@ -4,7 +4,8 @@ from api.models import Keyword
 
 
 class KeywordSerializer(serializers.ModelSerializer):
-
+    link = serializers.HyperlinkedIdentityField(view_name='keyword-detail', format='html')
+    
     class Meta:
         model = Keyword
-        fields = ('id', 'keyword',)
+        fields = ('id', 'keyword', 'link',)
