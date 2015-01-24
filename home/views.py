59cae548fca97ebce0a5dc6693dbd029e54ecca8
zeantsoi <zeantsoi@gmail.com>
2015-01-24 16:24:31 -0700

    Most functionality in place; need to dynamically pull from database

--- home/views.py
+++ home/views.py
@@ -11,7 +11,7 @@ def index(request):
     today = datetime.date.today()
     past = today - datetime.timedelta(weeks=1)
     today_str, past_str = today.strftime('%Y-%m-%d'), past.strftime('%Y-%m-%d')
-    url = 'https://www.federalregister.gov/api/v1/articles.json?per_page=100'
+    url = 'http://www.federalregister.gov/api/v1/articles.json?per_page=100&order=relevance&conditions%%5Bterm%%5D=obama|taxes&conditions%%5Bpublication_date%%5D%%5Bgte%%5D=%s&conditions%%5Bpublication_date%%5D%%5Blte%%5D=%s' % (past_str, today_str)
     r = requests.get(url)
     context = {'json': r.json()}
     return render(request, 'index.html', context)
