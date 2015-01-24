981820d7fe81e26349800d28859d98aff3d530b2
zeantsoi <zeantsoi@gmail.com>
2015-01-24 16:31:53 -0700

    Pulling from DB

--- home/views.py
+++ home/views.py
@@ -4,14 +4,24 @@ import requests
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting
+from api.models import Keyword
 
 # Create your views here.
 def index(request):
     today = datetime.date.today()
     past = today - datetime.timedelta(weeks=1)
     today_str, past_str = today.strftime('%Y-%m-%d'), past.strftime('%Y-%m-%d')
-    url = 'http://www.federalregister.gov/api/v1/articles.json?per_page=100&order=relevance&conditions%%5Bterm%%5D=obama|taxes&conditions%%5Bpublication_date%%5D%%5Bgte%%5D=%s&conditions%%5Bpublication_date%%5D%%5Blte%%5D=%s' % (past_str, today_str)
+    
+    keywords = []
+    for keyword in Keyword.objects.all():
+        keywords.append(keyword.keyword)
+
+
+
+
+    keywords_str = ('|').join(keywords)
+
+    url = 'http://www.federalregister.gov/api/v1/articles.json?per_page=100&order=relevance&conditions%%5Bterm%%5D=%s&conditions%%5Bpublication_date%%5D%%5Bgte%%5D=%s&conditions%%5Bpublication_date%%5D%%5Blte%%5D=%s' % (keywords_str, past_str, today_str)
     r = requests.get(url)
     context = {'json': r.json()}
     return render(request, 'index.html', context)
