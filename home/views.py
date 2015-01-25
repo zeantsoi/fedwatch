71f8420189793ebd3545e5f0c9b18ff9a1146f50
zeantsoi <zeantsoi@gmail.com>
2015-01-24 17:01:26 -0700

    Finalizing design assets

--- home/views.py
+++ home/views.py
@@ -12,16 +12,18 @@ def index(request):
     past = today - datetime.timedelta(weeks=1)
     today_str, past_str = today.strftime('%Y-%m-%d'), past.strftime('%Y-%m-%d')
     
-    keywords = []
-    for keyword in Keyword.objects.all():
-        keywords.append(keyword.keyword)
+    keywords = Keyword.objects.all()
+    keywords_list = []
+    for keyword in keywords:
+        keywords_list.append(keyword.keyword)
 
-
-
-
-    keywords_str = ('|').join(keywords)
+    keywords_str = ('|').join(keywords_list)
 
     url = 'http://www.federalregister.gov/api/v1/articles.json?per_page=100&order=relevance&conditions%%5Bterm%%5D=%s&conditions%%5Bpublication_date%%5D%%5Bgte%%5D=%s&conditions%%5Bpublication_date%%5D%%5Blte%%5D=%s' % (keywords_str, past_str, today_str)
     r = requests.get(url)
-    context = {'json': r.json()}
+
+    context = {'json': r.json(),
+               'start_date': past_str,
+               'end_date': today_str,
+               'keywords': keywords}
     return render(request, 'index.html', context)
